ffe0ef400dfad9f378197560001ddec204d49d57
AbdullahSahin1 <sahinabdullah2003@hotmail.com>
2025-07-21 15:10:45 +0300

    Sayfalar düzenlendi

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -2,7 +2,7 @@ export const NotFound = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Welcome to the Not Found Page</h1>
-      <p>This is the Not Found page of your React application.</p>
+      <p>This is the Not Found page of your React application.</p> 
     </div>
   );
 }
