1e1dc10a2b814c912dadf29ac7dc73d3576f53ad
AbdullahSahin1 <sahinabdullah2003@hotmail.com>
2025-07-21 16:23:33 +0300

    Gece Modu tasarlandı

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
+import { StarBackground } from "../Components/StarBackground";
 import { ThemeToggle } from "../Components/ThemeToggle";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         <ThemeToggle/>
+        <StarBackground/>
     </div>
   );
 }
