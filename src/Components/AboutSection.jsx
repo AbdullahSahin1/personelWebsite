6bb1cbc6a18c6a0a4a52a8649aad660a33c1d38b
AbdullahSahin1 <sahinabdullah2003@hotmail.com>
2025-07-22 13:39:13 +0300

    Tasarımda değişiklik yapıldı

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -12,7 +12,7 @@ export const AboutSection = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="space-y-6">
             <h3 className="text-2xl font-semibold">
-              Web Developer & Mobile Developer
+              Web Programlama & Mobil Programlama
             </h3>
 
             <p className="text-muted-foreground">
